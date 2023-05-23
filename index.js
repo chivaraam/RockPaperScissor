cbcb5568d7b1f016cb819200aab86fb258a4bd59
chivaraam <chivaraamayappan@gmail.com>
2023-05-23 20:49:51 +0530

    Finish computerChoice function

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const container = document.querySelector('#displayPoint');
 const buttons = document.querySelectorAll('#choices');
 
 function computerChoice(){
-    const choices = ["rock", "paper", "scissors"]
+    const choices = ["rock", "paper", "scissors"];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
 }
